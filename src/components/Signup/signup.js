b2ab56003c04995b60e0c8317b17b5609cfb8c85
BarnaTB <barnabastb2@gmail.com>
2019-03-03 02:00:43 +0300

    chore(Deploy the application to heroku): 	- The application should be deployed to Heroku. [Maintains #164099164]

--- src/components/Signup/signup.js
+++ src/components/Signup/signup.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SignUp extends Component{
     render() {
         return (
-            <h1>This is the signup page</h1>
+            <h1>Welcome to the signup page</h1>
         )
     }
 };
